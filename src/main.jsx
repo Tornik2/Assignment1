13ada4f00a05522ee06684d759a98952a4f79627
Tornik2 <tornik2@yahoo.com>
2024-09-21 12:34:51 +0400

    added Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import App from './App.jsx'
 import './index.css'
 

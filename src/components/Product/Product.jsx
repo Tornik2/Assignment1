9af0de53054770a97a7d169c997462a0df0c1800
Tornik2 <tornik2@yahoo.com>
2024-09-26 17:36:11 +0400

    added data for products and passed through props from main component to Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,14 +5,11 @@ export function Product(props) {
   return (
     <div className="product_wrapper">
       <div className="product_image_container">
-        <img src="/assets/1.png" alt="" className="product_image" />
+        <img src={props.src} alt="" className="product_image" />
       </div>
       <div className="product_all_info">
-        <h2 className="product_name">Saxeli</h2>
-        <p className="product_description">
-          AGWEr sassf asffsdddasffsdddasffsdddasffsdddasffsdddasffsddd asffsddd
-          asffsddd asffsddd shhuslkdjf; nameisme iunsn
-        </p>
+        <h2 className="product_name">{props.name}</h2>
+        <p className="product_description">{props.description}</p>
         <button className="add_btn">Add to Cart</button>
       </div>
     </div>

1eee1dc7c1dbc184fff2d05610556adde6863e47
Tornik2 <tornik2@yahoo.com>
2024-09-26 16:56:04 +0400

    added Prod

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,19 @@
+import "./Product.css";
+
+export function Product(props) {
+  return (
+    <div className="product_wrapper">
+      <div className="product_image_container">
+        <img src="/assets/1.png" alt="" className="product_image" />
+      </div>
+      <div className="product_all_info">
+        <h2 className="product_name">Saxeli</h2>
+        <p className="product_description">
+          AGWEr sassf asffsdddasffsdddasffsdddasffsdddasffsdddasffsddd asffsddd
+          asffsddd asffsddd shhuslkdjf; nameisme iunsn
+        </p>
+        <button className="add_btn">Add to Cart</button>
+      </div>
+    </div>
+  );
+}

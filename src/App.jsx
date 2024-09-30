83dc1405ef5e9fed4f9a3736eefe0b64f05230c8
Tornik2 <tornik2@yahoo.com>
2024-10-01 02:45:40 +0400

    remove content from home pagen and put assignment instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Assignment1 } from "./components/Assignment1";
-import { Content } from "./components/Main/MainContent";
 import { Layout } from "./components/Layout";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";

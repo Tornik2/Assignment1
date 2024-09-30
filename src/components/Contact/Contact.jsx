572fe19e192d91daa4da20bb79bb8bd1851cba7f
Tornik2 <tornik2@yahoo.com>
2024-10-01 00:15:31 +0400

    completed about page jsx and css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,8 @@ import "./Contact.css";
 
 export function Contact() {
   return (
-    <div className="contact_page">
+    <main className="contact_page">
       <h1>Contact PAGE</h1>
-    </div>
+    </main>
   );
 }

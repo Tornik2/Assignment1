01a1ea9ab347b8171bce824ef267b6fcfa7c1f08
Tornik2 <tornik2@yahoo.com>
2024-10-11 11:58:40 +0400

    add products fetching functionality

--- app/products/page.js
+++ app/products/page.js
@@ -1,10 +1,38 @@
+"use client";
 import "./products.css";
+import { useState, useEffect } from "react";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("https://dummyjson.com/products");
+
+        if (!res.ok)
+          throw new Error("Somthing went wrong with fetching Products");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Products not found!");
+        setProducts(data.products);
+        console.log(data.products[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <div className="products__wrapper"></div>
-      products
+      <div className="products__page__wrapper">
+        <div className="section_heading">
+          <img src="/assets/chevron-left.85606530.svg" alt="breadcrums-arrow" />
+          <h1 className="section_name">Products</h1>
+        </div>
+        <div className="products"></div>
+      </div>
     </>
   );
 }

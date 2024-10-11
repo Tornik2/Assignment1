7a6386b6124649c35ae6652e0d788cc59e7acc4d
Tornik2 <tornik2@yahoo.com>
2024-10-11 12:29:33 +0400

    display product cards on products page

--- app/components/product/page.js
+++ app/components/product/page.js
@@ -1,16 +1,25 @@
+import Link from "next/link";
 import "./Product.css";
 
-export function Product({ src, name, description }) {
+export function Product({ productObj }) {
+  const { src, name, description, id } = productObj;
+  console.log(productObj.src);
   return (
-    <div className="product_wrapper">
+    <li className="product_wrapper">
       <div className="product_image_container">
         <img src={src} alt="" className="product_image" />
       </div>
       <div className="product_all_info">
         <h2 className="product_name">{name}</h2>
         <p className="product_description">{description}</p>
-        <button className="add_btn">Add to Cart</button>
+        <Link
+          style={{ textAlign: "center" }}
+          href={`/products/${id}`}
+          className="add_btn"
+        >
+          ...Read More
+        </Link>
       </div>
-    </div>
+    </li>
   );
 }
--- app/products/page.js
+++ app/products/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { Product } from "../components/product/page";
 import "./products.css";
 import { useState, useEffect } from "react";
 
@@ -24,6 +25,17 @@ export default function Products() {
     fetchProducts();
   }, []);
 
+  const productList = products.map((product) => {
+    const { title, description, id, images } = product;
+    const productObj = {
+      name: title,
+      description: description,
+      id: id,
+      src: images[0],
+    };
+    return <Product productObj={productObj} key={id} />;
+  });
+
   return (
     <>
       <div className="products__page__wrapper">
@@ -31,7 +43,7 @@ export default function Products() {
           <img src="/assets/chevron-left.85606530.svg" alt="breadcrums-arrow" />
           <h1 className="section_name">Products</h1>
         </div>
-        <div className="products"></div>
+        <ul className="products__list">{productList}</ul>
       </div>
     </>
   );

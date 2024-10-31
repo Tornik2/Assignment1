f3751405a130ac18711671d43a62fbdb8eb1f824
Tornik2 <tornik2@yahoo.com>
2024-10-31 22:56:48 +0400

    make products page as protected route

--- app/products/page.js
+++ app/products/page.js
@@ -1,9 +1,14 @@
 import ProductsDetails from "./products";
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
+
 import { fetchProducts } from "./utils";
 
 import "./products.css";
 
-export default async function Products() {
-  const products = await fetchProducts();
-  return <ProductsDetails fetchedProducts={products} />;
-}
+export default withPageAuthRequired(
+  async function Products() {
+    const products = await fetchProducts();
+    return <ProductsDetails fetchedProducts={products} />;
+  },
+  { returnTo: "/api/auth/login" }
+);

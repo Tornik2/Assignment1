c0c3bfdbac231c4e78ca81fd02daee0cbc0802a8
Tornik2 <tornik2@yahoo.com>
2024-12-15 18:25:35 +0400

    install and test supabase

--- app/products/page.js
+++ app/products/page.js
@@ -1,5 +1,6 @@
 import ProductsDetails from "./products";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { supabase } from "../../utils/supabase/server";
 
 import { fetchProducts } from "./utils";
 
@@ -7,8 +8,11 @@ import "./products.css";
 
 export default withPageAuthRequired(
   async function Products() {
+    const { data } = await supabase.from("products").select("*");
     const products = await fetchProducts();
-    return <ProductsDetails fetchedProducts={products} />;
+    console.log("data:", data, products);
+
+    return <ProductsDetails fetchedProducts={data} />;
   },
   { returnTo: "/products" }
 );
